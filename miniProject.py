0c563bc2fbdd1cbacc9d14f46505a16127ffe162
ArinPaul <arinpaul56@gmail.com>
2020-11-21 00:49:53 +0530

    Python file

--- miniProject.py
+++ miniProject.py
@@ -4,11 +4,22 @@ import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
 
-data = '/Images'
+data = 'Images'
 category = ['MANGO']
+
+flat_data = []
+target = []
 
 for i in category:
     path = os.path.join(data, i)
-    print(path)
+    for img in os.listdir(path):
+        img_array = imread(os.path.join(path,img))
+        img_resize = resize(img_array,(50,50,3))
+        
+        flat_data.append(img_resize.flatten())
+        target.append(i)
+        
+    
+
 
 
